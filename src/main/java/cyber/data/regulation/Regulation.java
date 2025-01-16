2a414809c47dacbe723634f22146193fb8b67e98
zxp <15630703781@163.com>
2025-01-16 19:09:42 +0800

    init

---
+++ src/main/java/cyber/data/regulation/Regulation.java
@@ -0,0 +1,13 @@
+package cyber.data.regulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Regulation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Regulation.class, args);
+    }
+
+}
